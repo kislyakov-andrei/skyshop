69622283b22d44170accf8cab3a9dfc298fd4920
kislyakov-andrei <kisli@mail.ru>
2025-02-26 17:30:00 +0300

    initial commit

--- src/main/java/org/skypro/skyshop/model/service/StorageService.java
+++ src/main/java/org/skypro/skyshop/model/service/StorageService.java
@@ -1,5 +1,13 @@
 package org.skypro.skyshop.model.service;
 
-public class StorageService {
+import org.skypro.skyshop.model.article.Article;
+import org.skypro.skyshop.model.product.Product;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class StorageService {
+private final Map<UUID, Product> productMap= new HashMap<>();
+private final Map<UUID, Article> articleMap = new HashMap<>();
 }
